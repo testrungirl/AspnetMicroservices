feb636c3294033a556adbd9d2212aa5065205651
testrungirl <laigbodioh@gmail.com>
2022-11-17 04:26:12 +0100

    Added  action methods in ProdcutRepository file

---
+++ Source/Catalog/Catalog.Api/Repository/ProductRepository.cs
@@ -0,0 +1,42 @@
+﻿using Catalog.Api.Entitites;
+
+namespace Catalog.Api.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        public Task CreateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteProduct(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetProduct(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetProductByCategory(string categoryName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetProductByName(string productName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

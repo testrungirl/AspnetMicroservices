069661dd01b062ff833935ba0aed177587e75772
testrungirl <laigbodioh@gmail.com>
2022-11-25 23:31:57 +0100

    Corrected errors with  syntax

--- src/Services/Catalog/Catalog.Api/Data/CatalogContext.cs
+++ src/Services/Catalog/Catalog.Api/Data/CatalogContext.cs
@@ -9,7 +9,7 @@ namespace Catalog.Api.Data
         {
             var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
 
-            var database = client.GetDatabase(configuration.GetValue<string>("DatabseSettings:DatabaseName"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
 
             Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
 

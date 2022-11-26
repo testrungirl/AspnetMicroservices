35502cbeba6657d86335fc07b24d8742806a2ebc
testrungirl <laigbodioh@gmail.com>
2022-11-26 19:51:08 +0100

    Added ShoppingCart and ShoppingCartItem models

---
+++ src/Services/Basket/Basket.Api/Entities/ShoppingCart.cs
@@ -0,0 +1,24 @@
+﻿namespace Basket.Api.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName {get; set;}
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+        public ShoppingCart() { }
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalProce = 0;
+                foreach (ShoppingCartItem item in Items)
+                {
+                    totalProce += item.Price * item.Quantity;
+                }
+                return totalProce;
+            }
+    }
+}

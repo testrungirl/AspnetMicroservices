1b62c1902d498e8201b14a97671db812116fec89
testrungirl <laigbodioh@gmail.com>
2022-11-27 20:20:06 +0100

    Crated ShoppingCartItem Model

--- src/Services/Basket/Basket.Api/Entities/ShoppingCartItem.cs
+++ src/Services/Basket/Basket.Api/Entities/ShoppingCartItem.cs
@@ -2,10 +2,16 @@
 {
     public class ShoppingCartItem
     {
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         public string ProductId { get; set; }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         public string ProductName { get; set; }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         public int Quantity { get; set; }
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         public string Color { get; set; }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         public decimal Price { get; set; }
 
         

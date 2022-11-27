b1040d337d34ef01cf49970eb35bd7cde83ce8a5
testrungirl <laigbodioh@gmail.com>
2022-11-27 20:25:09 +0100

    Created RUD endpoints for ShoppingCart

---
+++ src/Services/Basket/Basket.Api/Controllers/BasketController.cs
@@ -0,0 +1,49 @@
+﻿using Basket.Api.Entities;
+using Basket.Api.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Basket.Api.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository _basketRepository;
+        public BasketController(IBasketRepository basketRepository)
+        {
+            _basketRepository = basketRepository ?? throw new ArgumentNullException(nameof(basketRepository));
+        }
+        // GET: api/<BasketController>
+        [HttpGet("{Name:length(24)}", Name ="GetBasket")]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<ActionResult<ShoppingCart>> GetBasketByName(string Name)
+        {
+            var cart = await _basketRepository.GetBasket(Name);
+            if(cart != null)
+            {
+                return Ok(cart);
+            }
+            return NotFound();
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> UpdateCart([FromBody] ShoppingCart cart)
+        {
+            return Ok(await _basketRepository.UpdateBasket(cart));
+        }
+
+        [HttpDelete("{userName}", Name = "DeleteBasket")]
+        [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> DeleteBasket(string userName)
+        {
+            await _basketRepository.DeleteBasket(userName);
+            return Ok();
+        }
+
+    }
+}

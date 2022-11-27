38425e3c9f3677f6b5de074ad507ef95fd825d53
testrungirl <laigbodioh@gmail.com>
2022-11-27 20:26:40 +0100

    Modified Swagger Dependency for Catalog API

--- src/Services/Catalog/Catalog.Api/Program.cs
+++ src/Services/Catalog/Catalog.Api/Program.cs
@@ -9,10 +9,26 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-// builder.Services.AddSwaggerGen(c=> {
-//     c.SwaggerDoc("v1", new openApiInfo {Title = "Catalog.API", Version = "v1"});
-// });
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Catalog API",
+        //Description = "An ASP.NET Core Web API for managing ToDo items",
+        //TermsOfService = new Uri("https://example.com/terms"),
+        //Contact = new OpenApiContact
+        //{
+        //    Name = "Example Contact",
+        //    Url = new Uri("https://example.com/contact")
+        //},
+        //License = new OpenApiLicense
+        //{
+        //    Name = "Example License",
+        //    Url = new Uri("https://example.com/license")
+        //}
+    });
+});
 builder.Services.AddScoped<ICatalogContext, CatalogContext>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
@@ -22,8 +38,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
-    // app.UseSwaggerUI(c=> c.SwaggerEndPoint("/swagger/v1/swagger.json", "Catalog.API v1"));
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog API V1");
+        //options.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseAuthorization();

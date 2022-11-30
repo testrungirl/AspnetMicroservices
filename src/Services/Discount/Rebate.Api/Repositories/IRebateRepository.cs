cc3de63951013a16a62806e3df88c05c31a6b31e
testrungirl <laigbodioh@gmail.com>
2022-11-30 13:53:32 +0100

    Added Coupon Interface

---
+++ src/Services/Discount/Rebate.Api/Repositories/IRebateRepository.cs
@@ -0,0 +1,12 @@
+﻿using Rebate.Api.Models;
+
+namespace Rebate.Api.Repositories
+{
+    public interface IRebateRepository
+    {
+        Task<Coupon> GetRebate(string productName);
+        Task<bool> CreateRebate(Coupon coupon);
+        Task<bool> DeleteRebate(string productName);
+        Task<bool> UpdateRebate(Coupon coupon);
+    }
+}

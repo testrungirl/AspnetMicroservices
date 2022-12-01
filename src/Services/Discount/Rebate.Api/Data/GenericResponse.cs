72e2c522820a3ba042de02604ce55fc08afe7dff
testrungirl <laigbodioh@gmail.com>
2022-12-01 07:14:32 +0100

    GenericResponse Class

---
+++ src/Services/Discount/Rebate.Api/Data/GenericResponse.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Rebate.Api.Data
+{
+    public class GenericResponse<T>
+    {
+        public T? Data { get; set; }
+        public bool Success { get; set; }
+        public string? Message { get; set; }
+    }
+
+}

d5af2ef4fb5b372877de8179a530851a7ba01a00
testrungirl <laigbodioh@gmail.com>
2022-12-04 09:33:24 +0100

    Registered Automapper classes

---
+++ src/Services/Discount/Rebate.GRPC/Mapper/RebateProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Rebate.GRPC.Models;
+using Rebate.GRPC.Protos;
+
+namespace Rebate.GRPC.Mapper
+{
+    public class RebateProfile: Profile
+    {
+        public RebateProfile()
+        {
+            CreateMap<Coupon, CouponModel>().ReverseMap();
+        }
+    }
+}

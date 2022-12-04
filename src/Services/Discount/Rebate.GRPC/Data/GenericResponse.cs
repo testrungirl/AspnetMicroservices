175355a9111e5159f16e4998ec4c66051a7551e1
testrungirl <laigbodioh@gmail.com>
2022-12-04 09:25:05 +0100

    added a generic response type for Rebate

---
+++ src/Services/Discount/Rebate.GRPC/Data/GenericResponse.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Rebate.GRPC.Data
+{
+    public class GenericResponse<T>
+    {
+        public T? Data { get; set; }
+        public bool Success { get; set; }
+        public string? Message { get; set; }
+    }
+
+}

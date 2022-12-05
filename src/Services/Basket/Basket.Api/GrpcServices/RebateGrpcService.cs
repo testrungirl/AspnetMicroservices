8dada20ce227be1bf8696765a4c2cb5ff9556287
testrungirl <laigbodioh@gmail.com>
2022-12-05 21:37:09 +0100

    Rebate Service

---
+++ src/Services/Basket/Basket.Api/GrpcServices/RebateGrpcService.cs
@@ -0,0 +1,21 @@
+﻿using Rebate.GRPC.Protos;
+
+namespace Basket.Api.GrpcServices
+{
+    public class RebateGrpcService
+    {
+        private readonly RebateProtoService.RebateProtoServiceClient _rebateProtoService;
+
+        public RebateGrpcService(RebateProtoService.RebateProtoServiceClient rebateProtoService)
+        {
+            Console.WriteLine("GRPC Client Instantiation");
+            _rebateProtoService = rebateProtoService;
+        }
+
+        public async Task<CouponModel> GetRebate(string productName)
+        {
+            var rebateRequest = new ProductName { Name = productName };
+            return await _rebateProtoService.GetDiscountAsync(rebateRequest);
+        }
+    }
+}

b89f5dab3b03fad4f063d14d59e43b88481f61fc
testrungirl <laigbodioh@gmail.com>
2022-12-10 13:43:29 +0100

    Used FluentValidation to Set RequiredFields for Create Checkout Method

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace Ordering.Application.Features.Orders.Commands.CheckoutOrder
+{
+    public class CheckoutOrderCommandValidator : AbstractValidator<CheckoutOrderCommand>
+    {
+        public CheckoutOrderCommandValidator()
+        {
+            RuleFor(p => p.UserName)
+                .NotEmpty().WithMessage("{UserName} is required")
+                .MaximumLength(50).WithMessage("{Username} must not exceed 50 characters.")
+                .NotNull();
+
+            RuleFor(p => p.EmailAddress)
+                .NotEmpty().WithMessage("{EmailAddress} is required.");
+
+            RuleFor(P => P.TotalPrice)
+                .NotEmpty().WithMessage("{TotalPrice} is required")
+                .GreaterThan(0).WithMessage("{TotalPrice} should be greater than 0");
+        }
+    }
+}

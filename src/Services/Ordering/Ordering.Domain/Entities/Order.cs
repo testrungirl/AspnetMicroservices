9b15d193a0c3e0a7b7d8f09416fc51440ca3af62
testrungirl <laigbodioh@gmail.com>
2022-12-10 09:05:29 +0100

    Added Property to Class

--- src/Services/Ordering/Ordering.Domain/Entities/Order.cs
+++ src/Services/Ordering/Ordering.Domain/Entities/Order.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Ordering.Domain.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,25 @@ using System.Threading.Tasks;
 
 namespace Ordering.Domain.Entities
 {
-    internal class Order
+    public class Order : EntityBase
     {
+        public string UserName { get; set; }
+        public decimal TotalPrice { get; set; }
+
+        // BillingAddress
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string AddressLine { get; set; }
+        public string Country { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+
+        // Payment
+        public string CardName { get; set; }
+        public string CardNumber { get; set; }
+        public string Expiration { get; set; }
+        public string CVV { get; set; }
+        public int PaymentMethod { get; set; }
     }
 }

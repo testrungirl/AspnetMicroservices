5971a9a5d72859acba2d534bbb415743e64c14f2
testrungirl <laigbodioh@gmail.com>
2022-12-10 10:39:42 +0100

    Added MediarR nugget package for to implement CQRS and Mediator Design Pattern

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrderListQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
+{
+    public class GetOrderListQuery : IRequest
+    {
+        public string UserName { get; set; }
+
+        public GetOrderListQuery(string userName)
+        {
+            UserName = userName ?? throw new ArgumentException(nameof(userName));
+        }
+    }
+}

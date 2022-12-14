e054d756f259449d91bb9b1d60b606fd46bac1f2
testrungirl <laigbodioh@gmail.com>
2022-12-14 20:15:17 +0100

    Inherited from DbContext

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext context, ILogger<OrderContextSeed> logger)
+        {
+            if (!context.Orders.Any())
+            {
+                context.Orders.AddRange(GetPreconfiguredOrders());
+                await context.SaveChangesAsync();
+                logger.LogInformation("Seed database associated with context {DbContextName}", typeof(OrderContext).Name);
+            }
+        }
+        public static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+            return new List<Order>()
+            {
+                new Order() { UserName = "swn", FirstName="Mehmet", LastName="Ozkaya", EmailAddress="egwakhidepeter@gmail.com", AddressLine="Lagos", Country = "Turkey", TotalPrice = 350}
+            };
+        }
+    }
+}
